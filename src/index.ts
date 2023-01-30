0eea988b7aa1ec445ee8a3178c6d7acd2f4db2ca
raydelv <100441535+raydelv@users.noreply.github.com>
2023-01-30 15:02:33 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ async function run() {
         const name = core.getInput('layer_name', { required: true })
         const zipFile = core.getInput('zip_file', { required: true })
         const runtimes = core.getInput('compatible_runtimes', { required: true})
+        
+        console.log(runtimes);
 
         const lambdaConfig: Lambda.Types.ClientConfiguration = {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
